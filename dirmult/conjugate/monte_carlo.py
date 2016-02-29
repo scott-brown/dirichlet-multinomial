228046a6ee229ff32b787fd8362c0bcd8f8a74d9
Scott Brown <sbrown103@gmail.com>
2016-02-29 16:35:46 -0500

    Added exception to handle missing data input to conjugate monte carlo sampler

--- dirmult/conjugate/monte_carlo.py
+++ dirmult/conjugate/monte_carlo.py
@@ -2,4 +2,7 @@
 import numpy as np
 
 def sampler(counts, prior_alpha, repl = 10000):
-    return np.random.dirichlet(counts.sum(0) + prior_alpha,repl)
+    if np.isnan(counts).any():
+        raise ValueError("Conjugate Monte Carlo sampler may only model fully non-missing count data.")
+    else:
+        return np.random.dirichlet(counts.sum(0) + prior_alpha,repl)
